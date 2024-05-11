9120f51befdf9643a090fd7be4db57f122d9edea
liujianjia <gxzpljj@163.com>
2024-05-11 15:44:52 +0800

    解决linux编译失败问题

--- lib/base64/include/base64.h
+++ lib/base64/include/base64.h
@@ -1,6 +1,7 @@
 #ifndef BASE64_H
 #define BASE64_H
-#include <stdint.h>
+#include <cstdint>
+#include <cstddef>
 
 char *encode_base64(const uint8_t *in, size_t len);
 uint8_t *decode_base64(const char *in, size_t len, size_t *olen);
