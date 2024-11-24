30b4e1c7cf0a1ace30305a72936f670da7d971ae
liujianjia <gxzpljj@163.com>
2024-11-24 10:38:07 +0800

    release 0.3.2

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define THISVERSION "0.3.1"
+#define THISVERSION "0.3.2"
 #define CHECKUPDATEURL "http://101.201.78.185:8080/api/checkUpdate"
 #define GETCDNURL "http://101.201.78.185:8080/api/getcdn"
 
